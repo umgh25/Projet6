d8c8c20fe163a73b0fa2969ec868f5f102c3a4b4
Usman Mughal <usman.mughal@outlook.fr>
2025-11-28 21:20:05 +0100

    feat: initialisation du modèle de données

---
+++ back/src/main/java/com/openclassrooms/mddapi/model/Post.java
@@ -0,0 +1,34 @@
+package com.openclassrooms.mddapi.model;
+
+import javax.persistence.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User author;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "topic_id", nullable = false)
+    private Topic topic;
+
+    private LocalDate createdAt;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments;
+}
