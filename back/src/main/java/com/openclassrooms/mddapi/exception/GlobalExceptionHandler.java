6ce6d31cbfd4969779bfb09560e738faefdcb71f
Usman Mughal <usman.mughal@outlook.fr>
2025-12-04 22:37:24 +0100

    feat(exception): Gestion des exceptions pour utilisateur existant

---
+++ back/src/main/java/com/openclassrooms/mddapi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.openclassrooms.mddapi.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserAlreadyRegisteredException.class)
+    public ResponseEntity<String> handleUserAlreadyRegisteredException(UserAlreadyRegisteredException ex) {
+        return new ResponseEntity<>("user already registered", HttpStatus.BAD_REQUEST);
+    }
+
+}
