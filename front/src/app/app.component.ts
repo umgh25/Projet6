eb4fddfe490b5bece32815ebc2fd0d5634b4d7ff
Usman Mughal <usman.mughal@outlook.fr>
2025-12-15 21:34:25 +0100

    feat(header): ajout du header et conditions d’affichage

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,24 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-import { filter } from 'rxjs/operators';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, Observable, of } from 'rxjs';
+import { SessionService } from './services/session.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
   title = 'front';
   showToolbar: boolean = true;
+  isMobileResolution = false;
+  isLogged: boolean = false;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private sessionService: SessionService) {}
 
   ngOnInit(): void {
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    ).subscribe(() => {
-      this.showToolbar = this.router.url !== "/";
+    this.isMobile();
+    this.sessionService.$isLogged().subscribe((response) => {
+      this.isLogged = response;
+      this.router.events
+        .pipe(filter((event) => event instanceof NavigationEnd))
+        .subscribe(() => {
+          this.displayToolBar();
+        });
     });
   }
 
+  private displayToolBar() {
+    this.showToolbar =
+      this.router.url !== '/' &&
+      ((!this.isLogged && !this.isMobileResolution) ||
+        (this.isLogged && this.isMobileResolution));
+  }
+
+  private isMobile() {
+    this.isMobileResolution = window.innerWidth <= 768;
+  }
 }
