ff0bc64819d68d0097419ad768c507945d96e002
Usman Mughal <usman.mughal@outlook.fr>
2025-12-17 20:22:17 +0100

    refactor(header): déplacer la logique d’affichage du header

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,47 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-import { filter } from 'rxjs';
-import { SessionService } from './services/session.service';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-
+export class AppComponent  {
   title = 'front';
-  showToolbar = true;
-  isMobileResolution = false;
-  isLogged = false;
-
-  constructor(
-    private router: Router,
-    private sessionService: SessionService
-  ) {}
-
-  ngOnInit(): void {
-    this.isMobile();
-
-    this.sessionService.$isLogged().subscribe((response: boolean) => {
-      this.isLogged = response;
-
-      this.router.events
-        .pipe(filter(event => event instanceof NavigationEnd))
-        .subscribe(() => {
-          this.displayToolBar();
-        });
-    });
-  }
-
-  private displayToolBar(): void {
-    this.showToolbar =
-      this.router.url !== '/' &&
-      ((!this.isLogged && !this.isMobileResolution) ||
-        (this.isLogged && this.isMobileResolution));
-  }
-
-  private isMobile(): void {
-    this.isMobileResolution = window.innerWidth <= 768;
-  }
 }
--- front/src/app/shared/components/header/header.component.ts
+++ front/src/app/shared/components/header/header.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { SessionService } from '../../../services/session.service';
+import { filter, Observable, of } from 'rxjs';
+import { NavigationEnd, Router } from '@angular/router';
+import { MobileService } from '../../../services/mobile.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  showToolbar: boolean = true;
+  isMobileResolution = false;
+  isLogged: boolean = false;
 
-  constructor() { }
+  constructor(private sessionService: SessionService, private router: Router, private mobileService: MobileService
+  ) {}
 
   ngOnInit(): void {
+    this.isMobile();
+
+    this.sessionService.$isLogged().subscribe((response) => {
+      this.isLogged = response;
+      this.router.events.pipe(
+        filter(event => event instanceof NavigationEnd)).subscribe(() => {
+        this.displayToolBar();
+      });
+    });
   }
 
+  logout() {
+    this.sessionService.logOut();
+    this.router.navigate(['/'])
+  }
+
+  private displayToolBar() {
+    this.showToolbar = (!this.isLogged && !this.isMobileResolution && (this.router.url.includes("register")) || this.router.url.includes("login")) || this.isLogged;
+    console.log((this.router.url.includes("register")) || this.router.url.includes("login"))
+  }
+
+  private isMobile() {
+    this.isMobileResolution = this.mobileService.isMobile();
+  }
 }
